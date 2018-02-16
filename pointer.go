e9d1213b49a676ec70f2f3c7652c7754e8b6b7a7
Gitesh Shinde <gitesh.shinde@devopstech.com>
2018-02-16 18:13:47 +0530

    pointers

---
+++ pointer.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// variable declared
+	x := 15
+
+	// storing memory address of x in a
+	a := &x
+
+	fmt.Println("Memory address : ", a)
+
+	// printing value of value at memory location a by *
+	fmt.Println("Value stored at a ", *a)
+
+	// assigning new value to variable using memory loction
+	*a = 5
+
+	// print x
+	fmt.Println("Value of x after chaanging value at memory location ", x)
+
+}
